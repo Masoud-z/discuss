e206b0db0606005c01b3b20043fa824c58e2ddbf
Masoud <masoud.zeinalabedini@gmail.com>
2024-04-26 11:21:07 +0330

    Add new files for comments

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,9 +1,27 @@
-interface Props {
-  params: { slug: string; postId: string };
+import Link from "next/link";
+import PostShow from "@/components/posts/post-show";
+import CommentList from "@/components/comments/comment-list";
+import CommentCreateForm from "@/components/comments/comment-create-form";
+import paths from "@/paths";
+
+interface PostShowPageProps {
+  params: {
+    slug: string;
+    postId: string;
+  };
 }
 
-const PostShowPage = ({ params: { slug, postId } }: Props) => {
-  return <div>PostShowPage</div>;
-};
+export default async function PostShowPage({ params }: PostShowPageProps) {
+  const { slug, postId } = params;
 
-export default PostShowPage;
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {"< "}Back to {slug}
+      </Link>
+      {/* <PostShow /> */}
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+      {/* <CommentList comments={comments} /> */}
+    </div>
+  );
+}
